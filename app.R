dac1c68e842c2dbeaf687d055919e0bdf54719ed
sfatihi24 <144145765+sfatihi24@users.noreply.github.com>
2023-12-12 18:39:33 -0500

    Updating shinyapp changes

--- app.R
+++ app.R
@@ -15,36 +15,7 @@ library(plotly)
 ###############
 
 #SENTIMENT LEXICON
-sentiment_data <- get_sentiments("afinn")
-
-#MOVIES
-movie1 <- read_csv("data/hp1.csv")
-movie2 <- read_csv("data/hp2.csv")
-movie3 <- read_csv("data/hp3.csv")
-movie4 <- read_csv("data/hp4.csv")
-movie5 <- read_csv("data/hp5.csv")
-movie6 <- read_csv("data/hp6.csv")
-movie7 <- read_csv("data/hp7.csv")
-movie8 <- read_csv("data/hp8.csv")
-
-movies_list <- list(movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8)
-
-movies <- movies_list |>
-  reduce(full_join)
-
-movies_all_words <- movies |>
-  unnest_tokens(output = word, input = dialog) |>
-  mutate(word = tolower(word)) |>
-  select(-character)
-
-rated_words <- movies_all_words |> 
-  inner_join(sentiment_data) |>
-  group_by(movie, chapter) |>
-  mutate(average_sentiment = mean(value)) |>
-  select(-word, -value) |>
-  distinct() |>
-  group_by(movie) |>
-  mutate(chapt_num = row_number())
+rated_words <- readRDS("shiny_rated_data.rds") #Reads in wrangled data set
 
 movie_name_choices <- unique(rated_words$movie)
 
@@ -80,17 +51,28 @@ server <- function(input, output) {
   # Creates filtered data set based on user input
   output$linegraph <- renderPlotly({
     selected_movies <- rated_words %>%
-      filter(movie %in% input$hp_choice)
+      filter(movie %in% input$hp_choice)  #Creates smaller data set based on users choices
+    
+    movie_colors <- c(
+      "Harry Potter and the Philosopher's Stone" = "#AE0001",
+      "Harry Potter and the Chamber of Secrets" = "#2A623D",
+      "Harry Potter and the Prisoner of Azkaban" = "#00165e",
+      "Harry Potter and the Goblet of Fire" = "#e3a000",
+      "Harry Potter and the Order of the Phoenix" = "#808080",
+      "Harry Potter and the Half-Blood Prince" = "black",
+      "Harry Potter and the Deathly Hallows Part 1" = "#9e711e",
+      "Harry Potter and the Deathly Hallows Part 2" = "royalblue3"
+    )
     
     # Creates graphic
     p <- ggplot(selected_movies, aes(x = chapt_num, y = average_sentiment, color = movie,
                                      text = paste0('Movie: ', movie, '\nChapter ', chapt_num , ': "', chapter, '"\n Average Sentiment: ', round(average_sentiment, 3)), group = 1)) +
       geom_line(aes(color = movie)) + 
-      geom_point() + 
-      scale_color_manual(values = c("#AE0001", "#2A623D", "#00165e", "#e3a000", "#808080", "black", "#9e711e", "orange")) + #NEED TO CHANGE COLORS
+      geom_point() +
+      scale_color_manual(values = movie_colors) + #uses harry potter themed color palette
       labs(x = "Chapter", y = "Average Sentiment", title = "Sentiment Analysis of the Harry Potter Film Series", color = "Movie")
     
-    p <- ggplotly(tooltip = c("text"))
+    p <- ggplotly(tooltip = c("text")) #adds hover tool to shiny app based on text in ggplot function
     
     return(p)
   })
